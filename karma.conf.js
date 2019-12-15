20feb55394154056fba1d8656155d49694f39b1d
Mgen <m@mgenware.com>
2019-12-15 23:42:00 +0800

    Add tests

---
+++ karma.conf.js
@@ -0,0 +1,20 @@
+/* eslint-disable import/no-extraneous-dependencies */
+const { createDefaultConfig } = require('@open-wc/testing-karma');
+const merge = require('webpack-merge');
+
+module.exports = config => {
+  config.set(
+    merge(createDefaultConfig(config), {
+      files: [
+        {
+          pattern: config.grep ? config.grep : 'tests/**/*.test.js',
+          type: 'module',
+        },
+      ],
+      esm: {
+        nodeResolve: true,
+      },
+    }),
+  );
+  return config;
+};
